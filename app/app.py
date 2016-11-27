69a1d793aa9cb8d3c8d2d4095ff2b58e79b4cbe9
Ruslan Khozinov <rkhozinov@mirantis.com>
2016-11-28 01:06:23 +0300

    app: trivial user interaction is added
    POST and get separated
    hellos count increases only by POST generated by user submit button click

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from redis import Redis
 from config import Config as cfg
 
@@ -8,10 +8,16 @@ app.config.from_object(cfg)
 redis = Redis(host=cfg.DB_HOST,
               port=cfg.DB_PORT)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def hellos_count():
-  redis.incr('hellos')
-  return 'hellos count {}'.format(redis.get('hellos'))
+    if request.method == 'POST':
+        redis.incr('hellos')
+        return redirect(url_for('hellos_count'))
+
+    return render_template('index.html',
+                           hellos_count=int(redis.get('hellos')))
+
 
 if __name__ == "__main__":
-  app.run(debug=cfg.DEBUG)
+    app.run(debug=cfg.DEBUG)
